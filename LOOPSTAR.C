dda96960cab2f1bd7421f5d6f94733c9c8313a55
rahul022022 <codewithrahul22@gmail.com>
2025-06-26 21:42:30 +0530

    26-6-2025

---
+++ LOOPSTAR.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,k;
+	clrscr();
+	for(i=0;i<10;i++)
+	{
+		for(k=0;k<9-i;k++)
+		{
+		printf(" ");
+		}
+		for(j=0;j<i;j++)
+		{
+		printf(" *");
+		}
+		printf("\n");
+	 }
+		printf("\n");
+
+	 for(i=10;i>0;i--)
+	 {
+		for(k=0;k<9-i;k++)
+		{
+		printf(" ");
+		}
+		for(j=0;j<i;j++)
+		{
+		printf(" *");
+		}
+		printf("\n");
+
+
+	}
+	getch();
+}
